6450e1e4131f0219534b0aebd21a9f630df84018
chhatrapal7 <cjanghel7@gmail.com>
2026-08-04 15:01:45 +0530

    Fix application-code folder structure completely

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.cdcm0s2ye4a1.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root123456',
+    DB_DATABASE : 'webappdb'
+});
